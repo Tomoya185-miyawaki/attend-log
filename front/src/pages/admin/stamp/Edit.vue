03ae9fbb84328a4f6ec9ebfb3524a01906025380
Tomoya <tomo16185@gmail.com>
2022-09-30 19:11:43 +0900

    [add]出退勤詳細の仮ページ作成

--- front/src/pages/admin/stamp/Edit.vue
+++ front/src/pages/admin/stamp/Edit.vue
@@ -2,23 +2,62 @@
   <HeaderComponent />
   <v-main>
     <v-container>
-      <v-row class="text-center">
-        <v-col cols="12">
-          出退勤詳細
-        </v-col>
-      </v-row>
+      <h2 class="mb-4">宮脇智也の出退勤状況</h2>
+      <FullCalendar :options="calendarOptions" />
     </v-container>
   </v-main>
+  <LoadingComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import '@fullcalendar/core/vdom'
+import FullCalendar from '@fullcalendar/vue3'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 import HeaderComponent from '@/components/layouts/HeaderComponent.vue'
+import LoadingComponent from '@/components/parts/LoadingComponent.vue'
 
 export default defineComponent({
   name: 'StampEditPage',
   components: {
     HeaderComponent,
+    LoadingComponent,
+    FullCalendar,
   },
+  setup() {
+    const calendarOptions = {
+      plugins: [ timeGridPlugin, interactionPlugin ],
+      initialView: 'timeGridWeek',
+      events: [
+        {
+          title: '勤務',
+          start: '2022-09-30 08:00:00',
+          end: '2022-09-30 12:00:00',
+          editable: true,
+        },
+        {
+          title: '休憩',
+          start: '2022-09-30 12:00:00',
+          end: '2022-09-30 13:00:00',
+          backgroundColor: '#a5f1c0',
+          borderColor: '#a5f1c0',
+          editable: true,
+        },
+        {
+          title: '出勤',
+          date: '2022-09-30 13:00:00',
+          end: '2022-09-30 17:00:00',
+          editable: true,
+        },
+      ],
+      nowIndicator: true,
+      locale: 'ja'
+    }
+
+    return {
+      calendarOptions
+    }
+  }
 })
 </script>
